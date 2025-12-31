011021688d3936c7ea05f531956a2cfc1462a47a
Rald Vibz <vibar_jherald@spcc.edu.ph>
2025-12-31 14:37:34 +0800

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { logos, navLinks } from '../constants'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav className='flex flex-row min-w-screen justify-between items-center px-10'>
+            <img src={logos[0].src} className='w-[150px] h-20' alt={logos[0].name} />
+
+            <ul className="flex flex-row gap-3">
+                {navLinks.map((link) => (
+                    <li key={link.label}>
+                        <a className='font-sans font-bold text-[.9rem] text-gray-700 px-2 py-2 rounded-lg hover:text-black hover:bg-gray-300' href={link.label}>{link.label}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+import Odds from "../assets/odds.png"
+
+export const navLinks = [
+    {label: "About"}, 
+    {label: "Donate"},
+    {label: "Contact"}
+]
+
+
+export const logos = [
+    {name: "Odds", src: Odds}
+]
